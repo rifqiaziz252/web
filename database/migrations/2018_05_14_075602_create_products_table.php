c06cf4bd4bebad27e6780dafca80e3543d50e20e
dzurrifqi aziz <rifqiaziz874@gmail.com>
2018-05-14 15:11:34 +0700

    aaa

--- database/migrations/2018_05_14_075602_create_products_table.php
+++ database/migrations/2018_05_14_075602_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nama');
-            $table->nim('password');
+            $table->string('password');
             $table->timestamps();
         });
     }
