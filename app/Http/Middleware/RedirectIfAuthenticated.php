e14cb082a5734c48a9ba95d07cf707c4f7c561ae
Ferdyansah <uchihaferdy123@gmail.com>
2018-06-30 19:38:42 +0700

    baru

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,22 +17,10 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        switch ($guard) {
-            case 'admin':
-                if (Auth::guard($guard)->check()){
-                    return redirect()->route('admin.home');
-                }
-                break;
-            
-            default:
-                 if (Auth::guard($guard)->check()){
-                    return redirect()->route('home');
-                }
-                break;
-        
+        if (Auth::guard($guard)->check()) {
+            return redirect('/home');
         }
 
         return $next($request);
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resource('v1', 'UserController');
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['prefix'=>'admin'],function(){
-Route::get('/login','Authadmin\LoginController@showLoginForm')->name('admin.login');
-Route::post('/login','Authadmin\LoginController@login')->name('admin.login.submit');
-Route::get('/', 'adminController@index')->name('admin.home');
-});
-
