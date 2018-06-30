2691b283ab4ea70da102146711ce5e3b01ea090d
Ferdyansah <uchihaferdy123@gmail.com>
2018-06-30 20:04:10 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+*/
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index');
+/*
+Route::get('/user', 'UserController@index');
+Route::get('/user-register', 'UserController@create');
+Route::post('/user-register', 'UserController@store');
+Route::get('/user-edit/{id}', 'UserController@edit');
+*/
+Route::resource('user', 'UserController');
+
+Route::resource('anggota', 'AnggotaController');
+
+Route::resource('buku', 'BukuController');
+Route::get('/format_buku', 'BukuController@format');
+Route::post('/import_buku', 'BukuController@import');
+
+Route::resource('transaksi', 'TransaksiController');
+Route::get('/laporan/trs', 'LaporanController@transaksi');
+Route::get('/laporan/trs/pdf', 'LaporanController@transaksiPdf');
+Route::get('/laporan/trs/excel', 'LaporanController@transaksiExcel');
+
+Route::get('/laporan/buku', 'LaporanController@buku');
+Route::get('/laporan/buku/pdf', 'LaporanController@bukuPdf');
+Route::get('/laporan/buku/excel', 'LaporanController@bukuExcel');
+
+
