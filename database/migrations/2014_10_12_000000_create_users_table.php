6ebc094f808bf863115a7c2213b4057bdccacb9b
JuniartiWahyuUtami <juniartiwahyuutami@gmail.com>
2018-07-02 12:46:28 +0700

    db user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username', 15)->unique();
+            $table->string('email');
             $table->string('password');
+            $table->string('gambar')->nullable();
+            $table->enum('level', ['admin', 'user']);
             $table->rememberToken();
             $table->timestamps();
         });
